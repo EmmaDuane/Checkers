0c9c89b44e434ad4d376de12e46a8d413ebe44d0
Ziyang Zhang <ziyangz5@ics.uci.edu>
2019-10-26 12:01:33 -0700

    Update Board.cpp

--- src/checkers-cpp/Board.cpp
+++ src/checkers-cpp/Board.cpp
@@ -437,8 +437,15 @@ void Board:: Undo(){
             
             this->board[x][y].color = (c==1?"B":"W");
             this->board[x][y].isKing = (k==0?false:true);
-            
-        }
+            if (c==1){
+                    this->blackCount += 1;
+                }
+                else{
+                    this->whiteCount += 1;
+                }
+                this->tieCount -= 1;
+
+            }
         saved_move_list.pop_back();
         
         
