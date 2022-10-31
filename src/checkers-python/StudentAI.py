b0bb44e70428b3a374d182d9a31c792c8b0c45c7
EmmaDuane <65099184+EmmaDuane@users.noreply.github.com>
2022-10-30 23:03:02 -0700

    added code for alpha beta pruning, not finished
    need to check if move is valid

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -2,7 +2,7 @@ from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
-#Students can modify anything except the class name and exisiting functions and varibles.
+#Students can modify anything except the class name and existing functions and variables.
 class StudentAI():
 
     def __init__(self,col,row,p):
@@ -14,28 +14,43 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-        
+
+# Useful functions from boardclass:
+#   is_valid_move(self, chess_row, chess_col, target_row, target_col, turn)
+#   is_win(self,turn)
         
 # Alpha-beta pruning:
-#   def ab_pruning(moves, move):
-#       max = max_val(moves, move, -inf, +inf)
-#       return move with max value
-#   def max_val(moves, move, alpha, beta):
-#       if end of game: return current move
-#       for i in moves:
-#           min = min_val(moves, i, alpha, beta)
-#           if min >= beta
-#               return +inf
-#           alpha = max(alpha, min)
-#       return alpha
-#   def min_val(moves, move, alpha, beta):
-#       if end of game: return current move
-#       for i in moves:
-#           min = max_val(moves, i, alpha, beta)
-#           if alpha >= min
-#               return -inf
-#           beta = min(beta, min)
-#       return beta
+def ab_pruning(moves, move):
+    max = max_val(moves, move, -sys.maxsize, maxsize)
+    return move # return move with max value
+
+
+def max_val(moves, move, alpha, beta):
+    # if end of game: return (state of current move, move)
+    if is_win(self, turn):
+        return sys.maxsize
+    return -sys.maxsize
+
+    for i in moves:
+        min = min_val(moves, i, alpha, beta)
+        if min >= beta:
+            return sys.maxsize
+        alpha = max(alpha, min)
+    return alpha
+
+
+def min_val(moves, move, alpha, beta):
+    # if end of game: return (state of current move, move)
+    if is_win(self, turn):
+        return -sys.maxsize
+    else:
+        return sys.maxsize
+    for i in moves:
+        min= max_val(moves, i, alpha, beta)
+        if alpha >= min:
+            return -sys.maxsize
+    beta = min(beta, min)
+    return beta
 
 
     def get_move(self,move):
@@ -44,8 +59,12 @@ class StudentAI():
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
+        # old code:
+            # index = randint(0,len(moves)-1)
+            # inner_index =  randint(0,len(moves[index])-1)
+
+            # index represents a checker, inner_index represents its possible moves
+            # move = moves[index][inner_index]
+        move = ab_pruning(moves, move)
         self.board.make_move(move,self.color)
         return move
