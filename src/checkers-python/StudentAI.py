a1a7df9f375a9697c5b5a0b2eecc7841455661ef
doancleon <74682100+doancleon@users.noreply.github.com>
2022-11-02 01:46:29 -0700

    1st heuristic

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -16,18 +16,39 @@ class StudentAI():
         self.color = 2
 
     def get_move(self, move):
+        #black always moves first and is always player 1
+        #self.color = 1 means AI is black
+        #self.color = 2 mean AI is white
+
+        #not sure what bottom code does
         if len(move) != 0:
             self.board.make_move(move, self.opponent[self.color])
         else:
             self.color = 1
+        #############################
         moves = self.board.get_all_possible_moves(self.color)
-        # old code:
-        #index = randint(0,len(moves)-1)
-        #inner_index =  randint(0,len(moves[index])-1)
-        #index represents a checker, inner_index represents its possible moves
-        #move = moves[index][inner_index]
-        #self.board.make_move(move, self.color)
-        #return move
+
+        #loops through all moves, whichever moves jumps more than once, use that move
+        jump = 2 #default is 2 since each move consists a list of at least length 2
+        temp = 0
+        for pieces in range(len(moves)):
+            for positions in range(len(moves[pieces])):
+                if len(moves[pieces][positions]) > jump:
+                    temp = moves[pieces][positions]
+                    jump = len(moves[pieces][positions])
+        if (temp != 0):
+            move = temp
+            self.board.make_move(move,self.color)
+            return move
+        else:
+            #pick random move:
+            index = randint(0,len(moves)-1)
+            inner_index =  randint(0,len(moves[index])-1)
+            #index represents a checker, inner_index represents its possible moves
+            move = moves[index][inner_index]
+            self.board.make_move(move, self.color)
+            return move
+        ##############################
         move = ab_pruning(moves, move)
         self.board.make_move(move, self.color)
         return move
@@ -36,13 +57,10 @@ class StudentAI():
 #   is_valid_move(self, chess_row, chess_col, target_row, target_col, turn)
 #   is_win(self,turn)
 
-#minimax algorithm
-def minimax(moves, move):
-    if is_win(self, turn):
-        return move
+
     # Alpha-beta pruning:
 def ab_pruning(moves, move):
-    max = max_val(moves, move, -sys.maxsize, maxsize)
+    #max = max_val(moves, move, -sys.maxsize, maxsize)
     return move # return move with max value
 
 
